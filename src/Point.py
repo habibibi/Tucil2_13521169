2b7d0d2375f6692c0fd7a4bad663066e148e8877
Muhammad Habibi Husni <habibihusni18@gmail.com>
2023-02-28 15:03:22 +0700

    feat: dnc fix added

--- src/Point.py
+++ src/Point.py
@@ -4,22 +4,29 @@ import random
 class Point:
     LIMIT = 1e5
     eucDistCnt = 0
-    def __init__(self, isRandom = False, dim = 3, arr = [0,0,0] ):
+    def __init__(self, dim = 3, arr = None):
         self.__r = []
-        if (isRandom):
-            self.__d = dim
-            for i in range(dim):
-                self.__r.append(random.uniform(-Point.LIMIT,Point.LIMIT))
-        else:
+        if (arr is not None):
             self.__d = len(arr)
             for i in range(len(arr)):
                 self.__r.append(arr[i])
+        else:
+            self.__d = dim
+            for i in range(dim):
+                self.__r.append(random.randint(-Point.LIMIT,Point.LIMIT))
+
 
     def getDim(self):
         return self.__d
 
     def getXi(self,i):
-        return self.__r[i]
+        if (i >= self.__d):
+            return 0
+        else:
+            return self.__r[i]
+        
+    def getFullCoor(self):
+        return self.__r
 
     def eucDist(self,p):
         ans = 0
@@ -36,7 +43,7 @@ class Point:
         l = []
         for i in range(self.__d):
             l.append(str(self.__r[i]))
-        return '('+','.join(l)+')'
+        return '['+','.join(l)+']'
     
     def __repr__(self):
         return str(self)
--- src/SpaceOfPoints.py
+++ src/SpaceOfPoints.py
@@ -1,31 +1,31 @@
 import PointSorter
 from Point import Point
-import time
-import matplotlib.pyplot as plt
+
 
 class SpaceOfPoints:
     INF = 1e9
-    def __init__(self,n,d = 3, points = None, sortedPoints = None):
-        if (points is None):
+    def __init__(self,d = 3, n = 10, points = None, arrPoints = None, fileDir = None):
+        if (fileDir is not None):
+            with open(fileDir) as f:
+                self.__n, self.__d = [int(x) for x in next(f).split()]
+                self.__points = []
+                for line in f:
+                    self.__points.append(Point(arr = [int(x) for x in line.split()]))
+        elif (arrPoints is not None):
+            self.__points = [Point(arr = list(arrPoints[i])) for i in range(len(arrPoints))]
+            self.__n = len(arrPoints)
+            self.__d = d
+        elif (points is not None):
+            self.__points = points
+            self.__n = len(points)
+            self.__d = d
+        else :
             self.__points = [None for i in range(n)]
             for i in range(n):
-                self.__points[i] = Point(True,d)
+                self.__points[i] = Point(d)
             self.__n = n
             self.__d = d
-            self.__sortedPoints = [None for i in range(d)]
-            st = time.time()
-            for i in range(d):
-                self.__sortedPoints[i] = self.__points.copy()
-                PointSorter.sort(self.__sortedPoints[i],i)
-            end = time.time()
-            self.initTime = end-st
-        else:
-            self.__points = points
-            self.__n = n
-            self.__d = d
-            self.__sortedPoints = sortedPoints
-            self.initTime = 0.0
-
+            
     def isEmpty(self):
         return self.__n == 0
 
@@ -35,95 +35,73 @@ class SpaceOfPoints:
     def getN(self):
         return self.__n
     
-    def filterPointsByIdx(self,l,r,di):
-        fPoints = self.__sortedPoints[di][l:r+1]
-        fSortedPoints = [[] for i in range(self.__d)]
-        # print(fPoints)
-        # print("-------------")
-        minX = self.__sortedPoints[di][l].getXi(di)
-        maxX = self.__sortedPoints[di][r].getXi(di)
-        for i in range(self.__d):
-            for j in range(self.__n):
-                if minX <= self.__sortedPoints[i][j].getXi(di) <= maxX:
-                    fSortedPoints[i].append(self.__sortedPoints[i][j])
-                    # print(self.__sortedPoints[i][j])
-            if (len(fPoints) != len(fSortedPoints[i])):
-                print(fPoints)
-                print(fSortedPoints[i])
-                print(len(fPoints),len(fSortedPoints[i]))        
-                assert len(fPoints) == len(fSortedPoints[i])
-            
-        return SpaceOfPoints(len(fPoints),self.__d,fPoints,fSortedPoints)
+    def getD(self):
+        return self.__d
+    
+    def getFilteredPointsByIdx(self,l,r,di):
+        fPoints = self.__points[l:r+1]
+        return SpaceOfPoints(len(fPoints),self.__d,fPoints)
 
-    def filterPointsByMinMax(self,minX,maxX,di):
+    def getFilteredPointsByMinMax(self,minX,maxX,di):
         fPoints = []
-        # print(self.__points)
         for i in range(self.__n):
             if minX <= self.__points[i].getXi(di) <= maxX:
-                # print(self.__points[i])
                 fPoints.append(self.__points[i])
-        # print("----------------")
-        fSortedPoints = [[] for i in range(self.__d)]
-        for i in range(self.__d):
-            for j in range(self.__n):
-                if minX <= self.__sortedPoints[i][j].getXi(di) <= maxX:
-                    # print(self.__sortedPoints[i][j])
-                    fSortedPoints[i].append(self.__sortedPoints[i][j])
-                
-            assert len(fPoints) == len(fSortedPoints[i])
-        
-        return SpaceOfPoints(len(fPoints),self.__d,fPoints,fSortedPoints)
+        return SpaceOfPoints(self.__d, points = fPoints)
 
-    def dnqClosestPair(self):
+    def dncClosestPair(self):
+        sortedPoint = self.__points.copy()
+        PointSorter.sort(sortedPoint,0)
+        return SpaceOfPoints(self.__d,points=sortedPoint).__dncClosestPairRec()
+
+    def __dncClosestPairRec(self):
         if (self.__n == 1) :
-            return SpaceOfPoints.INF
+            return SpaceOfPoints.INF, None, None
         elif (self.__n == 2):
-            return self.__sortedPoints[0][0].eucDist(self.__sortedPoints[0][1])
+            return self.__points[0].eucDist(self.__points[1]), self.__points[0], self.__points[1]
         else:
             mid = (self.__n-1)//2
-            qL = self.filterPointsByIdx(0,mid,0)
-            qR = self.filterPointsByIdx(mid+1,self.__n-1,0)
-            dL = qL.dnqClosestPair()
-            dR = qR.dnqClosestPair()
-            #print(qL)
-            #print(qR)
-            minDist = min(dL,dR)
-            #print(minDist)
-            xMid = (self.__sortedPoints[0][0].getXi(0) + self.__sortedPoints[0][self.__n-1].getXi(0))/2
-            #print("x mid = ", xMid)
-            qMid = self.filterPointsByMinMax(xMid-minDist,xMid+minDist,0)
-            #print("qmid = ",qMid)
-            dMid = qMid.__findClosestSparse(0,minDist)
-            return min(minDist,dMid)
+            qL = self.getFilteredPointsByIdx(0,mid,0)
+            qR = self.getFilteredPointsByIdx(mid+1,self.__n-1,0)
+            dL, pLA, pLB = qL.__dncClosestPairRec()
+            dR, pRA, pRB = qR.__dncClosestPairRec()
+            if (dL < dR):
+                pA = pLA
+                pB = pLB
+                minDist = dL
+            else:
+                pA = pRA
+                pB = pRB
+                minDist = dR
+            xMid = self.__points[mid].getXi(0)
+            qMid = self.getFilteredPointsByMinMax(xMid-minDist,xMid+minDist,0)
+            dMid, pMidA, pMidB = qMid.__findClosestSparse(minDist)
+            if (dMid < minDist):
+                pA = pMidA
+                pB = pMidB
+                minDist = dMid            
+            return minDist, pA, pB
 
-    def __findClosestSparse(self,curD,minRange):
+    def __findClosestSparse(self,minRange):
         if (self.isEmpty()) :
             return SpaceOfPoints.INF
-        elif (curD == self.__d-1):
+        else:
             minDist = SpaceOfPoints.INF
+            pA = None
+            pB = None
+            sortedPoints = self.__points.copy()
+            PointSorter.sort(sortedPoints,self.__d-1)
             for i in range(self.__n):
                 j = i+1
-                now = self.__sortedPoints[curD][i]
-                while (j < self.__n and now.getXi(curD)-self.__sortedPoints[curD][j].getXi(curD) < minRange):
-                    minDist = min(minDist,now.eucDist(self.__sortedPoints[curD][j]))
+                now = sortedPoints[i]
+                while (j < self.__n and sortedPoints[j].getXi(self.__d-1)-now.getXi(self.__d-1) < minRange):
+                    dist = now.eucDist(sortedPoints[j])
+                    if (dist < minDist):
+                        minDist = dist
+                        pA = now
+                        pB = sortedPoints[j]
                     j += 1
-            return minDist
-        else:
-            leftX = self.__sortedPoints[curD][0].getXi(curD)
-            rightX = self.__sortedPoints[curD][self.__n-1].getXi(curD)
-            if (rightX-leftX <= 2*minRange):
-                return self.__findClosestSparse(curD+1,minRange)
-            else:
-                mid = (self.__n-1)//2
-                qL = self.filterPointsByIdx(0,mid,curD)
-                qR = self.filterPointsByIdx(mid+1,self.__n-1,curD)
-                dL = qL.__findClosestSparse(curD,minRange)
-                dR = qR.__findClosestSparse(curD,minRange)
-                minDist = min(dL,dR)
-                xMid = (self.__sortedPoints[curD][0].getXi(0) + self.__sortedPoints[curD][self.__n-1].getXi(0))/2
-                qMid = self.filterPointsByMinMax(xMid-minRange,xMid+minRange,curD)
-                dMid = qMid.__findClosestSparse(curD,minRange)
-                return min(minDist,dMid)
+            return minDist, pA, pB
             
     def bruteClosestPair(self):
         minDist = SpaceOfPoints.INF
@@ -132,32 +110,17 @@ class SpaceOfPoints:
                 minDist = min(minDist,self.__points[i].eucDist(self.__points[j]))
         return minDist
     
+    def printToFile(self,fileDir):
+        f = open(fileDir,'w')
+        print(self.__n,self.__d,file=f)
+        for i in range(self.__n):
+            for j in range(self.__d):
+                print(self.__points[i].getXi(j),end=' ',file=f)
+            print(file=f)
+
+
     def __str__(self):
         return str(self.__points)
     
     def __repr__(self):
         return str(self)
-
-
-hehe = SpaceOfPoints(20,3)
-st = time.time()
-print("brute = ",hehe.bruteClosestPair())
-end = time.time()
-print(end-st)
-print(Point.eucDistCnt)
-
-Point.resetEucDistCnt()
-
-st = time.time()
-print("hasil = ",hehe.dnqClosestPair())
-end = time.time()
-print(end-st+hehe.initTime)
-print(Point.eucDistCnt)
-
-fig = plt.figure(figsize=(4,4))
-ax = fig.add_subplot(111, projection='3d')
-for i in range(hehe.getN()):
-    p = hehe.getPoints()[i]
-    ax.scatter(p.getXi(0),p.getXi(1),p.getXi(2),c='blue') # plot the point (2,3,4) on the figure
-
-plt.show()
